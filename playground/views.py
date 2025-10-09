f9b861b113c56193115fbee79ce34d4f95e33adc
Spp77 <sppatil7070@gmail.com>
2025-10-10 02:56:16 +0530

    Fix import errors and setup admin panel

--- playground/views.py
+++ playground/views.py
@@ -1,1 +1,9 @@
+from django.shortcuts import render
 from .models import Product
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'playground/home.html', { 'products': products })
+
+def say_hello(request):
+    return render(request, 'playground/hello.html', {'name': 'Zoro'})
